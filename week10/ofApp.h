2deafc8fe93dda6127a37d2e276f4ca486b139d6
kimbeomjoon99 <102931473+kimbeomjoon99@users.noreply.github.com>
2023-09-03 14:20:31 +0900

    Add files via upload

---
+++ week10/ofApp.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    /* WaterFall-related member variables Regions */
+    
+    // flag variables
+    int draw_flag;
+    int load_flag;
+    
+    // Line segment and dot related variables
+    int num_of_line, num_of_dot;
+    float dot_diameter;
+
+	//week1
+	int* linex1;
+	int* linex2;
+	int* liney1;
+	int* liney2;
+	int* dotx1;
+	int* doty1;
+	int dot_location;
+
+	//week2
+	int water_flag;
+	float *linslope;
+	float Wx;
+	float Wy;
+	int sel_dot_change;
+	float* draw_water_spotx;
+	float* draw_water_spoty;
+
+    /* WaterFall-related member functions */
+    
+    void processOpenFileSelection(ofFileDialogResult openFileResult);
+    void initializeWaterLines(); // 2nd week portion.
+		
+};
